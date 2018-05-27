3d0327ea73ff141bad77f7997cabb00077ec2517
Miguel A. Magjistari <Arber@iMac.local>
2018-05-27 19:59:12 +0200

    Added crud actions to the Vehicle class

---
+++ src/Form/VehicleType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Vehicle;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VehicleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type')
+            ->add('seats')
+            ->add('model')
+            ->add('price')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Vehicle::class,
+        ]);
+    }
+}
